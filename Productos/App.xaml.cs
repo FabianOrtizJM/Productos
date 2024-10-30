7eb497af8b09b05e2b2a663f5f11986dff8187f6
Fabian Ortiz (Jackypaws) <garfiun18@gmail.com>
2024-10-29 19:18:17 -0600

    uwu

---
+++ Productos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Productos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Productos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Productos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
